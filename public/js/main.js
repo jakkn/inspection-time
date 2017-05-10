06400118f34588f00d82a8657d107e478c8ca921
Jakob Knutsen <jakobknutsen@gmail.com>
2017-05-10 18:55:10 +0200

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+InspectionTime = function(){
+    const $img = $("#flashing-image"),
+        $btnLeft = $("#btn-left"),
+        $btnRight = $("#btn-right"),
+        leftImage = "img/inspection-time-left.jpg",
+        rightImage = "img/inspection-time-right.jpg",
+        maskImage = "img/inspection-time-mask.jpg",
+        $labelCorrect = $("#label-correct"),
+        $labelWrong = $("#label-wrong"),
+        $labelTotal = $("#label-total");
+    var activeDirection = "left",
+        correct = 0,
+        wrong = 0,
+        total = 0;
+
+    $btnLeft.on( "click", {side: "left"}, handleClick );
+    $btnRight.on( "click", {side: "right"}, handleClick );
+
+    function handleClick(e){
+        total++;
+        if( e.data.side === activeDirection )
+            correct++;
+        else
+            wrong++;
+
+        updateStats();
+        displayNewImage();
+    }
+
+    function updateStats(){
+        $labelCorrect.text(correct);
+        $labelWrong.text(wrong);
+        $labelTotal.text(total);
+    }
+
+    function displayNewImage(){
+        if(Math.floor(Math.random() * 2) > 0) {
+            activeDirection = "right";
+            displayImage(rightImage);
+        }
+        else {
+            activeDirection = "left";
+            displayImage(leftImage);
+        }
+
+        setTimeout(displayImage, 300, maskImage);
+    }
+
+    function displayImage(image){
+        $img.attr("src", image);
+    }
+
+}();
